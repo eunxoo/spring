6782450cdeecf52553a2c2bb85f9cb52ab2a904f
eunxoo <dmstn7989@naver.com>
2022-07-09 21:17:08 +0900

    week04

---
+++ week04/main/java/com/sparta/week04/controller/ProductRestController.java
@@ -0,0 +1,36 @@
+package com.sparta.week04.controller;
+
+import com.sparta.week04.dto.ProductRequestDto;
+import com.sparta.week04.models.Product;
+import com.sparta.week04.models.ProductRepository;
+import com.sparta.week04.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProductRestController {
+
+    private final ProductService productService;
+    private final ProductRepository productRepository;
+
+    // 등록된 전체 상품 목록 조회
+    @GetMapping("/api/products")
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    // 신규 상품 등록
+    @PostMapping("/api/products")
+    public Product createProduct(@RequestBody ProductRequestDto requestDto) {
+        Product product = new Product(requestDto);
+        productRepository.save(product);
+        // 응답 보내기
+        return product;
+    }
+}
---
+++ week04/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -0,0 +1,49 @@
+package com.sparta.week04.utils;
+
+import com.sparta.week04.dto.ItemDto;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component // 이제부터, @RequiredArgsConstructor와 함께 사용할 경우 스프링이 자동으로 생성합니다.
+public class NaverShopSearch {
+    public String search(String query){
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Naver-Client-Id", "9GYWFUJjR_t97B7H54Kk");
+        headers.add("X-Naver-Client-Secret", "E_7Fsw0hpa");
+        String body = "";
+
+        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=" + query, HttpMethod.GET, requestEntity, String.class);
+        HttpStatus httpStatus = responseEntity.getStatusCode();
+        int status = httpStatus.value();
+        String response = responseEntity.getBody();
+        System.out.println("Response status: " + status);
+        System.out.println(response);
+
+        return response;
+    }
+
+    public List<ItemDto> fromJSONtoItems(String result) {
+        JSONObject rjson = new JSONObject(result); // 문자열 정보를 JSONObject로 바꾸기
+        JSONArray items  = rjson.getJSONArray("items"); // JSONObject에서 items 배열 꺼내기
+        List<ItemDto> ret = new ArrayList<>();
+        for (int i=0; i<items.length(); i++) { //JSONArray로 for문 돌기
+            JSONObject itemJson = items.getJSONObject(i);
+            ItemDto itemDto = new ItemDto(itemJson);
+            ret.add(itemDto);
+        }
+        return ret;
+    }
+
+        public static void main(String[] args){
+        NaverShopSearch naverShopSearch = new NaverShopSearch();
+        naverShopSearch.search("아이맥");
+        }
+}
