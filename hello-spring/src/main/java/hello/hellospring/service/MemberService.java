010a7faf34bf31dfe7f058ce91ed423c096690db
eunxoo <dmstn7989@naver.com>
2023-02-26 23:26:13 +0900

    컴포넌트 스캔과 자동 의존관계 설정
    @Component
         -@Controller
         -@Service
         -@Repository

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,16 +3,20 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService  {
 
     private final MemberRepository memberRepository;
 
     // test에서 객체를 생성하면 이 객체와 다른 리포지토리를 사용하게 되어, 아래와 같은 생성자를 통해 같은 리포지토리를 사용할 수 있게 함.
     // 외부에서 리포지토리를 넣어줌 -> DI Dependency Injection
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
